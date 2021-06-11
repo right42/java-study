51d62d986988a0c39f0859c0640053eea439e94f
right42 <chjm452@naver.com>
2021-06-11 15:25:58 +0900

    equals and hashcode study

---
+++ src/study/EqualsAndHashcodeTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class EqualsAndHashcodeTest {
+
+	public static void main(String[] args) {
+		User user1 = new User("test", 23);
+		User user2 = new User("test", 23);
+
+		System.out.println(user1.equals(user2));
+
+		// Map<User, String> userMaps = Map.of(user1, "test", user2, "test2");
+		// System.out.println(userMaps);
+	}
+
+	static class User {
+		private String name;
+
+		private int age;
+
+		public User(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			User user = (User)o;
+			return age == user.age && Objects.equals(name, user.name);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(name, age);
+		}
+	}
+
+}
